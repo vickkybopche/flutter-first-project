316edb36acc0800be2a567f5821f13ebc8bbfa6e
vickkybopche <vickkybopche2017@gmail.com>
2021-07-14 23:13:09 +0530

    Day3

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,12 +6,53 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login_Page",style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.brown
-        ),),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+            Padding(padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 80),
+            child: Column(children: [
+              TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter username",
+                labelText: "Username"
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter password",
+                labelText: "Password"
+              ),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text("Login"),
+              style: TextButton.styleFrom(), 
+               )
+            ],),
+            ),
+          
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_app/Pages/home_page.dart';
 import 'package:flutter_app/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main(){
   runApp(MyApp());
 }
@@ -12,13 +13,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         // primarySwatch: Colors.red
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
         "/":(context)=> LoginPage(),
         "/home":(context)=> HomePage(),
