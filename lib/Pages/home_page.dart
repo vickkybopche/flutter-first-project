9b4c8ce4ba17d7ebe9e7f95274884516e3bb14fc
vickkybopche <vickkybopche2017@gmail.com>
2021-07-20 19:40:10 +0530

    multiple models create

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/catalog.dart';
 import 'package:flutter_app/widgets/drawer.dart';
+import 'package:flutter_app/widgets/item_widget.dart';
 
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
      
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body:Center(
-          child: Text("Welcome to vs code Flutter"),
-      ) ,
+      body:Padding
+      (
+        padding: EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context,index){
+            return ItemWidget(item:dummyList[index],);
+          },
+        ),
+      ),
       drawer:MyDrawer(),
     );
   }
